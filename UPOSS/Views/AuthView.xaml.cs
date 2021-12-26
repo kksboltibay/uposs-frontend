8e01642af6c94ddeace7bcc179a78d18eff645c8
Chris <chrischoo.official@gmail.com>
2021-12-26 16:27:47 +0800

    Fix login page auto focus bug

--- UPOSS/Views/AuthView.xaml.cs
+++ UPOSS/Views/AuthView.xaml.cs
@@ -22,7 +22,12 @@ namespace UPOSS.Views
         public AuthView()
         {
             InitializeComponent();
-            TxtB.Focus();
+            Loaded += AuthView_Loaded;
+        }
+
+        private void AuthView_Loaded(object sender, RoutedEventArgs e)
+        {
+            tbUsername.Focus();
         }
 
         private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
