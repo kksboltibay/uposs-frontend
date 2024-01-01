89a6a31b4e9fd2856b67199fbd143ddb50373213
Chris <chrischoo.official@gmail.com>
2024-01-01 13:24:49 +0800

    tune cashier font size and add company no

---
+++ UPOSS/Controls/Dialog/CustomMessageDialog.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UPOSS.Controls.Dialog
+{
+    /// <summary>
+    /// Interaction logic for CustomMessageDialog.xaml
+    /// </summary>
+    public partial class CustomMessageDialog : Window
+    {
+        public CustomMessageDialog(string text)
+        {
+            InitializeComponent();
+
+            tbkMessage.Text = text;
+        }
+
+        private void btnDialogOk_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+    }
+}
